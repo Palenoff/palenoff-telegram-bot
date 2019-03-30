f1d1cf493c0e1f3785cbf08b5eb4f755f91ff2be
Kirill Palenov <kpalenov@mail.ru>
2019-03-31 01:53:39 +0300

    Code uploaded

---
+++ Palenoff-bot/bot.py
@@ -0,0 +1,74 @@
+import telebot
+import responses
+import keyboards
+import commands
+import logging
+import datetime
+import os
+
+bot = telebot.AsyncTeleBot(os.environ["TOKEN"])
+logging.basicConfig(filename='messages.log')
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.from_user.id, "Привет!\nЯ Бот Палёныч!\nКирилл решил не утрждать себя вопросами самопрезентации и доверил эту высокую миссию!\nЧто ж, надеюсь, я сумею оправдать Ваши ожидания😏\nПожелайте мне удачи!🙏", reply_markup=keyboards.start_markup)
+
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    bot.send_message(message.from_user.id,responses.responses["помощь"]["reply"])
+
+@bot.message_handler(commands=['about'])
+def handle_about(message):
+    bot.send_message(message.from_user.id,responses.responses["о боте"]["reply"])
+
+def send_message(message, response):
+    bot.send_message(message.chat.id,response["reply"],reply_markup=response["markup"])
+
+def handle_command(message,messagetext):
+    if messagetext == "помощь":
+        handle_help(message)
+    elif messagetext == "о боте":
+        handle_about(message)
+    elif messagetext == "назад":
+        handle_back(message)
+    else:
+        send_message(message,responses.responses[messagetext])
+
+
+@bot.message_handler(content_types=['text'])
+def handle_message(message):
+    print(datetime.datetime.now())
+    print(message.json)
+    logging.debug(datetime.datetime.now())
+    logging.debug(message.json)
+    messagetext = message.text.lower()
+    if messagetext in responses.responses.keys():
+        print("Команда: " + messagetext)
+        handle_command(message,messagetext)
+    else:
+        print("Текст: " + messagetext)
+        reply = commands.parse_text(messagetext)
+        if reply is None:
+            reply = "На данный момент я не обладаю подобной информацией!\nНо не переживайте!\nВаш вопрос записан в лог, и скоро я узнаю ответ на него!"
+            logging.info(messagetext)
+        print("Ответ: " + reply)
+        bot.send_message(message.chat.id, reply)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.message:
+        print(datetime.datetime.now())
+        print(call.message.json)
+        logging.debug(datetime.datetime.now())
+        logging.debug(call.message.json)
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=responses.responses[call.data]["reply"],reply_markup=responses.responses[call.data]["markup"])
+            #bot.send_message(chat_id=call.message.chat.id, text="Сообщение отправилось из нормального сообщения об образовании")
+
+if __name__ == '__main__':
+    try:
+        bot.polling(none_stop=True)
+    except Exception as ex:
+        print(datetime.datetime.now())
+        print(ex.args)
+        logging.critical(datetime.datetime.now())
+        logging.critical(ex.args)
---
+++ Palenoff-bot/keyboards.py
@@ -0,0 +1,26 @@
+from telebot.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+start_markup = ReplyKeyboardMarkup(True)
+start_markup.row("Профессиональные качества","Персональные качества");
+start_markup.row("Помощь","О боте");
+
+professional_qualities_markup = InlineKeyboardMarkup()
+education_callback_button = InlineKeyboardButton(text="Образование", callback_data="образование")
+skills_callback_button = InlineKeyboardButton(text="Навыки", callback_data="навыки")
+experience_callback_button = InlineKeyboardButton(text="Опыт", callback_data="опыт")
+projects_callback_button = InlineKeyboardButton(text="Проекты", callback_data="проекты")
+professional_qualities_markup.row(education_callback_button,skills_callback_button)
+professional_qualities_markup.row(experience_callback_button, projects_callback_button)
+
+
+personal_qualities_markup = InlineKeyboardMarkup()
+hobbies_callback_button = InlineKeyboardButton(text="Хобби", callback_data="хобби")
+languages_callback_button = InlineKeyboardButton(text="Языки", callback_data="языки")
+personality_callback_button = InlineKeyboardButton(text="Личные качества", callback_data="личные качества")
+personal_qualities_markup.row(hobbies_callback_button,languages_callback_button)
+personal_qualities_markup.row(personality_callback_button)
+
+skills_markup = InlineKeyboardMarkup()
+c_sharp_skills_button = InlineKeyboardButton(text="C#", callback_data="c#")
+java_skills_button = InlineKeyboardButton(text="Java", callback_data="java")
+python_skills_button = InlineKeyboardButton(text="Python", callback_data="python")
+skills_markup.row(c_sharp_skills_button,java_skills_button,python_skills_button)
