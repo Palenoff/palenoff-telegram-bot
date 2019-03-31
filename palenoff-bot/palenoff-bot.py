7a083348b0148fc7fabccd1552842f3f1a222f54
Kirill Palenov <kpalenov@mail.ru>
2019-03-31 14:25:45 +0300

    Logging modified

--- palenoff-bot/palenoff-bot.py
+++ palenoff-bot/palenoff-bot.py
@@ -2,12 +2,10 @@ import telebot
 import responses
 import keyboards
 import commands
-import logging
-import datetime
 import os
 
 bot = telebot.AsyncTeleBot(os.environ["TOKEN"])
-logging.basicConfig(filename='messages.log')
+#logging.basicConfig(filename='messages.log')
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
@@ -37,30 +35,31 @@ def handle_command(message,messagetext):
 
 @bot.message_handler(content_types=['text'])
 def handle_message(message):
-    print(datetime.datetime.now())
+    print("Зарос:")
     print(message.json)
-    logging.debug(datetime.datetime.now())
-    logging.debug(message.json)
+    #logging.debug(datetime.datetime.now())
+    #logging.debug(message.json)
     messagetext = message.text.lower()
     if messagetext in responses.responses.keys():
-        print("Команда: " + messagetext)
+        print("Распознан как Команда: " + messagetext)
         handle_command(message,messagetext)
     else:
-        print("Текст: " + messagetext)
+        print("Распознан как Текст: " + messagetext)
         reply = commands.parse_text(messagetext)
         if reply is None:
             reply = "На данный момент я не обладаю подобной информацией!\nНо не переживайте!\nВаш вопрос записан в лог, и скоро я узнаю ответ на него!"
-            logging.info(messagetext)
+            print("Текст не распознан")
+            #logging.info(messagetext)
         print("Ответ: " + reply)
         bot.send_message(message.chat.id, reply)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if call.message:
-        print(datetime.datetime.now())
+        print("Инлайновый запрос:")
         print(call.message.json)
-        logging.debug(datetime.datetime.now())
-        logging.debug(call.message.json)
+        #logging.debug(datetime.datetime.now())
+        #logging.debug(call.message.json)
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=responses.responses[call.data]["reply"],reply_markup=responses.responses[call.data]["markup"])
             #bot.send_message(chat_id=call.message.chat.id, text="Сообщение отправилось из нормального сообщения об образовании")
 
@@ -68,9 +67,8 @@ if __name__ == '__main__':
     try:
         bot.polling(none_stop=True)
     except Exception as ex:
-        print(datetime.datetime.now())
         print(ex.args)
-        logging.critical(datetime.datetime.now())
-        logging.critical(ex.args)
+        #logging.critical(datetime.datetime.now())
+        #logging.critical(ex.args)
 
 #bot.polling(none_stop=True,interval=0)
